9af55fa4511024dfbe60c3503dcb49641bb632a4
FromAlaska <Jim_samson1335@yahoo.com>
2017-10-10 16:52:16 -0800

    Added comments/changes

--- 2017/Fall/CS311/Counterhsr/counthsr.cpp
+++ 2017/Fall/CS311/Counterhsr/counthsr.cpp
@@ -1,18 +1,22 @@
 // Jim Samson
 // October 09, 2017
 // counthsr.cpp
-// This file is a
+// This is for Assignment 4
 
 #include "counthsr.h"
 
+// countHSR()
+// See header for details.
 int countHSR(int dim_x, int dim_y, int hole_x, int hole_y, int start_x, int start_y, int finish_x, int finish_y)
 {
 	if(dim_x == 0 || dim_y == 0)
 		return 0;
 
+	// Creates a new 2-Dimensional array, then sets a counter to zero.
 	board2DArray newBoard;
 	int count = 0;
 
+	// Sets the dimenstions, starting postion, hole position of the board.
 	newBoard.resize(dim_x, vector<int>(dim_y,0));
 	newBoard[start_x][start_y] = 1;
 	newBoard[hole_x][hole_y] = 1;
@@ -21,9 +25,11 @@ int countHSR(int dim_x, int dim_y, int hole_x, int hole_y, int start_x, int star
 	return countHSR_recurse(newBoard, dim_x, dim_y, finish_x, finish_y, start_x, start_y, count);
 }
 
+// countHSR_recurse()
+// See header for details.
 int countHSR_recurse(board2DArray & boardArray, int dim_x, int dim_y, int finish_x, int finish_y, int pos_x, int pos_y, int squaresLeft)
 {
-	if (squaresLeft == 0 && pos_x == finish_x && pos_y == finish_y)
+	if(squaresLeft == 0 && pos_x == finish_x && pos_y == finish_y)
 	{
         return 1;
   }
@@ -31,98 +37,139 @@ int countHSR_recurse(board2DArray & boardArray, int dim_x, int dim_y, int finish
 	// Recursive
 	int total = 0;
 
-	//move right
-  if ((pos_x+1 < dim_x) && boardArray[pos_x+1][pos_y] == 0)
+	// Move Right
+  if((pos_x+1 < dim_x) && boardArray[pos_x+1][pos_y] == 0)
 	{
+			// This moves the position of the x & y axis and adds a one to the current spot in the array.
       pos_x++;
       boardArray[pos_x][pos_y] = 1;
       squaresLeft--;
+
+			// Add to the total amount of recursive calls.
       total += countHSR_recurse(boardArray, dim_x, dim_y, finish_x, finish_y, pos_x, pos_y, squaresLeft);
+
+			// Reset current position
       boardArray[pos_x][pos_y] = 0;
       pos_x--;
       squaresLeft++;
   }
-  //move left
-  if (((0 <= pos_x-1 && pos_x-1 < dim_x)) && boardArray[pos_x-1][pos_y] == 0)
+  // Move Left
+	//
+  if(((0 <= pos_x-1 && pos_x-1 < dim_x)) && boardArray[pos_x-1][pos_y] == 0)
 	{
+			// This moves the position of the x & y axis and adds a one to the current spot in the array.
       pos_x--;
       boardArray[pos_x][pos_y] =1;
       squaresLeft--;
+
+			// Add to the total amount of recursive calls.
       total += countHSR_recurse(boardArray, dim_x, dim_y, finish_x, finish_y, pos_x, pos_y, squaresLeft);
+
+			// Reset current position
       boardArray[pos_x][pos_y] = 0;
       pos_x++;
       squaresLeft++;
   }
-  //move up
-  if ((pos_y+1 < dim_y) && boardArray[pos_x][pos_y+1] == 0)
+  // Move Up
+  if((pos_y+1 < dim_y) && boardArray[pos_x][pos_y+1] == 0)
 	{
+			// This moves the position of the x & y axis and adds a one to the current spot in the array.
       pos_y++;
       boardArray[pos_x][pos_y] =1;
       squaresLeft--;
+
+			// Add to the total amount of recursive calls.
       total += countHSR_recurse(boardArray, dim_x, dim_y, finish_x, finish_y, pos_x, pos_y, squaresLeft);
+
+			// Reset current position
       boardArray[pos_x][pos_y] = 0;
       pos_y--;
       squaresLeft++;
   }
-  //move down
-  if ((0 <= pos_y-1 && pos_y-1 < dim_y) && boardArray[pos_x][pos_y-1] == 0)
+  // Move Down
+  if((0 <= pos_y-1 && pos_y-1 < dim_y) && boardArray[pos_x][pos_y-1] == 0)
 	{
+			// This moves the position of the x & y axis and adds a one to the current spot in the array.
       pos_y--;
       boardArray[pos_x][pos_y] =1;
       squaresLeft--;
+
+			// Add to the total amount of recursive calls.
       total += countHSR_recurse(boardArray, dim_x, dim_y, finish_x, finish_y, pos_x, pos_y, squaresLeft);
+
+			// Reset current position
       boardArray[pos_x][pos_y] = 0;
       pos_y++;
       squaresLeft++;
   }
 
-  //move right up
-  if ((pos_x+1 < dim_x) && (pos_y+1 < dim_y) && boardArray[pos_x+1][pos_y+1] == 0)
+  // Move Right Up
+  if((pos_x+1 < dim_x) && (pos_y+1 < dim_y) && boardArray[pos_x+1][pos_y+1] == 0)
 	{
+			// This moves the position of the x & y axis and adds a one to the current spot in the array.
       pos_x++;
       pos_y++;
       boardArray[pos_x][pos_y] =1;
       squaresLeft--;
+
+			// Add to the total amount of recursive calls.
       total += countHSR_recurse(boardArray, dim_x, dim_y, finish_x, finish_y, pos_x, pos_y, squaresLeft);
+
+			// Reset current position
       boardArray[pos_x][pos_y] = 0;
       pos_x--;
       pos_y--;
       squaresLeft++;
   }
-  //move left up
-  if (((0 <= pos_x-1) && (pos_y+1 < dim_y)) && boardArray[pos_x-1][pos_y+1] == 0)
+  // Move Left Up
+  if(((0 <= pos_x-1) && (pos_y+1 < dim_y)) && boardArray[pos_x-1][pos_y+1] == 0)
 	{
+			// This moves the position of the x & y axis and adds a one to the current spot in the array.
       pos_x--;
       pos_y++;
       boardArray[pos_x][pos_y] =1;
       squaresLeft--;
+
+			// Add to the total amount of recursive calls.
       total += countHSR_recurse(boardArray, dim_x, dim_y, finish_x, finish_y, pos_x, pos_y, squaresLeft);
+
+			// Reset current position
       boardArray[pos_x][pos_y] = 0;
       pos_x++;
       pos_y--;
       squaresLeft++;
   }
-  //move right down
-  if ((pos_x+1 < dim_x) && (0 <= pos_y-1) && boardArray[pos_x+1][pos_y-1] == 0)
+  // Move Right Down
+  if((pos_x+1 < dim_x) && (0 <= pos_y-1) && boardArray[pos_x+1][pos_y-1] == 0)
 	{
+			// This moves the position of the x & y axis and adds a one to the current spot in the array.
       pos_x++;
       pos_y--;
       boardArray[pos_x][pos_y] =1;
       squaresLeft--;
+
+			// Add to the total amount of recursive calls.
       total += countHSR_recurse(boardArray, dim_x, dim_y, finish_x, finish_y, pos_x, pos_y, squaresLeft);
+
+			// Reset current position
       boardArray[pos_x][pos_y] = 0;
       pos_x--;
       pos_y++;
       squaresLeft++;
   }
-  //move left down
-  if ((0 <= pos_x-1 && pos_x-1 < dim_x) && (0 <= pos_y-1 && pos_y-1 < dim_y) && boardArray[pos_x-1][pos_y-1] == 0)
+  // Move Left Down
+  if((0 <= pos_x-1 && pos_x-1 < dim_x) && (0 <= pos_y-1 && pos_y-1 < dim_y) && boardArray[pos_x-1][pos_y-1] == 0)
 	{
+			// This moves the position of the x & y axis and adds a one to the current spot in the array.
       pos_y--;
       pos_x--;
       boardArray[pos_x][pos_y] =1;
       squaresLeft--;
+
+			// Add to the total amount of recursive calls.
       total += countHSR_recurse(boardArray, dim_x, dim_y, finish_x, finish_y, pos_x, pos_y, squaresLeft);
+
+			// Reset current position
       boardArray[pos_x][pos_y] = 0;
       pos_y++;
       pos_x++;
--- 2017/Fall/CS311/Counterhsr/counthsr.h
+++ 2017/Fall/CS311/Counterhsr/counthsr.h
@@ -1,22 +1,28 @@
 // Jim Samson
 // October 09, 2017
 // counthsr.hpp
-//
-
-#include <vector>
-
-using std::vector;
+// This is for Assignment 4.
 
 #ifndef COUNTHSR_FILE
 #define COUNTHSR_FILE
 
+#include <vector>
+using std::vector;
+
 typedef vector<vector<int>> board2DArray;
 
-int countHSR(int dim_x, int dim_y,
-             int hole_x, int hole_y,
-             int start_x, int start_y,
-             int finish_x, int finish_y);
+// countHSR
+// This function returns the number of possible choices that the spider can take.
+// It returns the recursive function of the x amount of times it took to make it to the finish.
+// Pre:
+//      dim_x and dim_y cannot be zero.
+int countHSR(int dim_x, int dim_y, int hole_x, int hole_y, int start_x, int start_y, int finish_x, int finish_y);
 
+// countHSR_recurse()
+// This function is to check if the spider can jump onto the next square diagonally, up, down, left, right.
+// It also checks if the current position is equal to the final position, or if the squares that are left is zero.
+// Pre:
+//			The number of squares that are left cannot be negative.
 int countHSR_recurse(board2DArray & boardArray, int dim_x, int dim_y, int finish_x, int finish_y, int pos_x, int pos_y, int squaresLeft);
 
 #endif  //COUNTHSR_FILE
