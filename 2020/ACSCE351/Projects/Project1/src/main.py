e70a8e766dbb39f67be1f8b988264a6cce8ac8c2
fromalaska <thealaskanfrontier@gmail.com>
2020-01-20 14:15:32 -0900

    modified stuff

--- 2020/ACSCE351/Projects/Project1/src/main.py
+++ 2020/ACSCE351/Projects/Project1/src/main.py
@@ -3,19 +3,44 @@
 import sys
 
 buffArray = []
+stackArray = []
 state = None
 
 STATE_ADD = None
 
+#Evaluate things
+def evaluateFunction(operand_2,operand_1,element):
+    if(element == "+"):
+        return int(operand_2) + int(operand_1)
+    if(element == "-"):
+        return int(operand_2) - int(operand_1)
+    if(element == "*"):
+        return int(operand_2) * int(operand_1)
+
 #Basic Parsing Algorithm
 def parse(inputString):
-    print("Input: " + inputString)
+    buffArray = inputString #Make a copy of the stuff
+    print("Input: " + buffArray)
 
-    for totalNum in inputString:
-        #buffArray.append(totalNum)
-        if(totalNum ==):
-            print("stuff")
-
+    for indexElement in buffArray:
+        #buffArray.append(indexElement)
+        if(indexElement is "+" or 
+           indexElement is "-" or 
+           indexElement is "(" or 
+           indexElement is ")" or
+           indexElement is "*"):
+            print("TOKEN (operator): " + indexElement)
+            operand_2 = stackArray.pop()
+            operand_1 = stackArray.pop()
+            result = evaluateFunction(operand_2, operand_1, indexElement)
+            print("RESULT: " + str(result))
+        if(indexElement is not "+" and 
+           indexElement is not "-" and 
+           indexElement is not "(" and 
+           indexElement is not ")" and
+           indexElement is not "*"):
+            print("TOKEN: " + indexElement)
+            stackArray.append(indexElement)
     print(buffArray)
 
 if __name__ == "__main__":
