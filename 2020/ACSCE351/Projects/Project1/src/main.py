3f7c7c33e7f83697c0a31452c8b9494044eee6c5
Jim Samson <jasamson@alaska.edu>
2020-01-21 08:27:38 -0900

    made changes

--- 2020/ACSCE351/Projects/Project1/src/main.py
+++ 2020/ACSCE351/Projects/Project1/src/main.py
@@ -4,9 +4,9 @@ import sys
 
 buffArray = []
 stackArray = []
-state = None
-
-STATE_ADD = None
+operatorIsTrue = False
+ifOperand=False
+ifParanthesis=False
 
 #Evaluate things
 def evaluateFunction(operand_2,operand_1,element):
@@ -24,24 +24,25 @@ def parse(inputString):
 
     for indexElement in buffArray:
         #buffArray.append(indexElement)
-        if(indexElement is "+" or 
-           indexElement is "-" or 
-           indexElement is "(" or 
-           indexElement is ")" or
-           indexElement is "*"):
-            print("TOKEN (operator): " + indexElement)
-            operand_2 = stackArray.pop()
-            operand_1 = stackArray.pop()
-            result = evaluateFunction(operand_2, operand_1, indexElement)
-            print("RESULT: " + str(result))
-        if(indexElement is not "+" and 
-           indexElement is not "-" and 
-           indexElement is not "(" and 
-           indexElement is not ")" and
-           indexElement is not "*"):
-            print("TOKEN: " + indexElement)
-            stackArray.append(indexElement)
+        if(indexElement == "("):
+            if(indexElement is "+" or 
+                indexElement is "-" or 
+                indexElement is ")" or
+                indexElement is "*"):
+                    ifOperand=False
+
+            if(indexElement is not "+" and 
+                indexElement is not "-" and 
+                indexElement is not "(" and 
+                indexElement is not ")" and
+                indexElement is not "*"):
+                    ifOperand=True
+                    print("TOKEN: " + indexElement)
+                    stackArray.append(indexElement)
+        if(indexElement == ")"):
+            print("stuff")
     print(buffArray)
+
 
 if __name__ == "__main__":
     stringInput = input("Enter the stuff: ")
