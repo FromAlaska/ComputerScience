f014f8a46ea2fe0dc21df133a6017ccc8f023b80
fromalaska <thealaskanfrontier@gmail.com>
2020-02-21 00:57:27 -0900

    added stuff

--- 2020/ACSCE351/Homework/HW1/hw1.py
+++ 2020/ACSCE351/Homework/HW1/hw1.py
@@ -4,12 +4,27 @@
 # Homework 1 
 # This is a homework assignment for CSCE 351. 
 # Included is the transition function 
-# for the DFA class. It is also used for main.
+# for the DFA class. 
 
 from automata import *
 
-def transition():
-    pass
+""" ********(Delta Function)********
+    delta is either a function taking a state and a 1-character
+    string in argument (positional arguments in this order) and
+    returning either a state or a frozenset of states (see above) 
+    or a dictionary mapping pairs (q,a) of a state q and a 1-character
+    string a to a state or to a frozenset of states (see above)
+
+    delta must be defined on all possible combinations of states
+    and symbols in the alphabet, i.e. the automaton must have a 
+    transition with every symbol in the alphabet for every state, 
+    possibly to a capture state.
+"""
+def transition(q, a):
+    state = q
+    if state == "0":
+        return "(1)
+
 
 # MAIN
 if __name__ == "__main__":
