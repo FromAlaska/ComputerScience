9ca082ccbc20ad3ff435461e743bb48c3011e082
fromalaska <jasamson@alaska.edu>
2020-04-25 20:27:51 -0800

    finished everything

--- 2020/ACSCE351/Homework/HW3/knuthSearch.sh
+++ 2020/ACSCE351/Homework/HW3/knuthSearch.sh
@@ -6,17 +6,21 @@ echo "(2) best case method (search for 'ab' that has various cases of 'ab'"
 echo "(3) best case method (search for 'bee' in BeeMovieScript)"
 echo "(4) worst case method (random string search)"
 echo "(5) rewrite randomly generated string"
-echo "(6) exit"
+echo "(6) worst case method (multiple a's)"
+echo "(7) exit"
+echo "(8) rewrite multiple occurance generated string"
 
 while true; do
-    read -p "(6 to exit)>>" ans
+    read -p "(7 to exit)>>" ans
     case $ans in
         1 ) ./knuth ./Scripts/beeOnlyScript bee;;
         2 ) ./knuth ./Scripts/abScript ab;;
         3 ) ./knuth ./Scripts/BeeMovieScript bee;;
         4 ) read -p "Put in string to search: " str; ./naive ./Scripts/randomlyGeneratedString $str;;
-        5 ) rm -rf ./Scripts/randomlyGeneratedString && touch ./Scripts/randomlyGeneratedString ; echo "rewriting randomly generated string"; python2 ./src/randomcharactergenerator.py;;
-        6 ) exit;;
-        * ) echo "Please answer one of the following options. (6) to exit";;
+        5 ) rm -rf ./Scripts/randomlyGeneratedString && touch ./Scripts/randomlyGeneratedString ; echo "rewriting randomly generated string"; python2 ./randomcharactergenerator.py;;
+        6 ) ./knuth ./Scripts/multipleOccuranceCharacter aab;;
+        7 ) exit;;
+        8 ) rm -rf ./Scripts/multipleOccuranceCharacter && touch ./Scripts/multipleOccuranceCharacter ; echo "rewriting multiple occurance generated string"; python2 ./src/multipleOccuranceAgenerator.py;;
+        * ) echo "Please answer one of the following options. (7) to exit";;
     esac
 done
---
+++ 2020/ACSCE351/Homework/HW3/src/multipleOccuranceAgenerator.py
@@ -0,0 +1,7 @@
+import string
+import random
+
+with open("/home/spacecookies/Documents/gitFiles/ComputerScience/2020/ACSCE351/Homework/HW3/Scripts/multipleOccuranceCharacter", "a") as my_file:
+	for i in range(1,1000):
+		my_file.write("a")
+	my_file.write("b")
